7757dfc94aeea09ae547c4c67f291ce208bdbe9e
ttilley02 <56703674+ttilley02@users.noreply.github.com>
2020-08-12 16:59:06 -0400

    cleanup of extra components

---
+++ src/components/LanguageChoice.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Main1 extends Component {
+  render() {
+    return (
+      <>
+        <h2>Spanish VocabuLab</h2>
+      </>
+    );
+  }
+}
