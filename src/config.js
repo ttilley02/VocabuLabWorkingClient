de77f96c9cb57f0768d6af8e35b6d14cae085b0d
ttilley02 <56703674+ttilley02@users.noreply.github.com>
2020-08-13 16:24:14 -0400

    initial commit

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: `http://localhost:8080`
-  // API_ENDPOINT: `https://shielded-sea-55188.herokuapp.com`
+  // API_ENDPOINT: `http://localhost:8080`
+  API_ENDPOINT: `https://shielded-sea-55188.herokuapp.com`
 };
